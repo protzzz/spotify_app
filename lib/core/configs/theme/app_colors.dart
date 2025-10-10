816d9ef13713926d27bbb3a6691d3c4beea739cf
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-11 00:16:31 +0200

    Add lightGrey color to AppColors

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static const darkBackground = Color(0xff0D0C0C);
   static const grey = Color(0xffBEBEBE);
   static const darkGrey = Color(0xff343434);
+  static const lightGrey = Color(0xffdadada);
 }
