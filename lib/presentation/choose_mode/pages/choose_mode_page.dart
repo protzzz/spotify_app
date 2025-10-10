a2eff67d337d7b732d7fd1c88914880ebc860e68
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-10 04:33:21 +0200

    Add ChooseModePage widget

---
+++ lib/presentation/choose_mode/pages/choose_mode_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ChooseModePage extends StatelessWidget {
+  static const String routeName = '/choose-mode';
+  const ChooseModePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Choose Mode Page')));
+  }
+}
