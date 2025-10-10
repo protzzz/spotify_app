84ce46314ae26cbce185b9c86bfeaff5d6d9bf46
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-11 00:15:44 +0200

    Add choose-mode-background image asset

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -3,4 +3,6 @@ class AppImages {
 
   static const String introBackground =
       '${basePath}intro-background.png';
+  static const String chooseModeBackground =
+      '${basePath}choose-mode-background.png';
 }
