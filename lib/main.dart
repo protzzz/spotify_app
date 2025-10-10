fd8879ab8276e101f7b28813d9641a002dadc0b4
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-11 00:16:52 +0200

    Integrate HydratedBloc and ThemeCubit for state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:spotify_app/core/configs/theme/app_theme.dart';
+import 'package:spotify_app/presentation/choose_mode/cubit/theme_cubit.dart';
 import 'package:spotify_app/presentation/splash/pages/splash_page.dart';
 import 'package:spotify_app/router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory:
+        kIsWeb
+            ? HydratedStorageDirectory.web
+            : HydratedStorageDirectory(
+              (await getTemporaryDirectory()).path,
+            ),
+  );
   runApp(const MyApp());
 }
 
@@ -12,12 +26,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: (settings) => generateRoute(settings),
-      title: 'Flutter Demo',
-      theme: AppTheme.lightTheme,
-      home: const SplashPage(),
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (context) => ThemeCubit())],
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder:
+            (context, mode) => MaterialApp(
+              debugShowCheckedModeBanner: false,
+              onGenerateRoute: (settings) => generateRoute(settings),
+              title: 'Flutter Demo',
+              theme: AppTheme.lightTheme,
+              home: const SplashPage(),
+            ),
+      ),
     );
   }
 }
