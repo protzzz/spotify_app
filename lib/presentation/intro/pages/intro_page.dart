6b0d9e10f3f773e2fbfd3439893040b696a19c6d
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-10 04:34:09 +0200

    Add IntroPage for app introduction screen

---
+++ lib/presentation/intro/pages/intro_page.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_app/common/widgets/basic_app_button.dart';
+import 'package:spotify_app/core/configs/assets/app_images.dart';
+import 'package:spotify_app/core/configs/assets/app_vectors.dart';
+import 'package:spotify_app/core/configs/theme/app_colors.dart';
+import 'package:spotify_app/presentation/choose_mode/pages/choose_mode_page.dart';
+
+class IntroPage extends StatelessWidget {
+  static const String routeName = '/intro-page';
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.introBackground),
+                fit: BoxFit.fill,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.15),
+                  BlendMode.darken,
+                ),
+              ),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30),
+                  child: Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(AppVectors.logo),
+                  ),
+                ),
+                Spacer(),
+                Text(
+                  'Enjoy Listening To Music',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 22,
+                  ),
+                ),
+                SizedBox(height: 21),
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.grey,
+                    fontSize: 16,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                BasicAppButton(
+                  onPressed: () {
+                    Navigator.pushNamedAndRemoveUntil(
+                      context,
+                      ChooseModePage.routeName,
+                      (_) => false,
+                    );
+                  },
+                  title: 'Get Started',
+                ),
+                SizedBox(height: 15),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
