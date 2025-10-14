f59e5eb8d48df2737a0eff7d8c09223eb0e79139
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-15 00:15:49 +0200

    Add ChooseAuth page for authentication selection

---
+++ lib/presentation/auth/pages/choose_auth.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_app/common/helpers/is_dark_mode.dart';
+import 'package:spotify_app/common/widgets/auth_appbar.dart';
+import 'package:spotify_app/common/widgets/basic_app_button.dart';
+import 'package:spotify_app/core/configs/assets/app_images.dart';
+import 'package:spotify_app/core/configs/assets/app_vectors.dart';
+
+class ChooseAuth extends StatelessWidget {
+  static const String routeName = '/choose-auth';
+  const ChooseAuth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          AuthAppbar(),
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(AppVectors.topLines),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset(AppVectors.bottomLines),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset(AppImages.bottomAuthImage),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 27),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(AppVectors.logo),
+                  SizedBox(height: 40),
+                  Text(
+                    'Enjoy Listening To Music',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 28,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    'Spotify is a proprietary Swedish audio streaming and media services provider ',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 17,
+                      color:
+                          Theme.of(context).brightness ==
+                                  Brightness.dark
+                              ? Color(0xffD7D7D7)
+                              : Color(0xff797979),
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: BasicAppButton(
+                          onPressed: () {},
+                          title: 'Register',
+                          fontSize: 19,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 19,
+                              color:
+                                  context.isDarkMode
+                                      ? Color(0xffD7D7D7)
+                                      : Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
