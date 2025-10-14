3760d7cac19caaad526e469e6efb1f56afcf8d32
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-15 00:15:59 +0200

    Implement JSON serialization for ThemeCubit

--- lib/presentation/choose_mode/cubit/theme_cubit.dart
+++ lib/presentation/choose_mode/cubit/theme_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:meta/meta.dart';
 
 part 'theme_state.dart';
 
@@ -11,13 +10,29 @@ class ThemeCubit extends HydratedCubit<ThemeMode> {
 
   @override
   ThemeMode? fromJson(Map<String, dynamic> json) {
-    // TODO: implement fromJson
-    throw UnimplementedError();
+    // switch (json['theme'] as String?) {
+    //   case 'light':
+    //     return ThemeMode.light;
+    //   case 'dark':
+    //     return ThemeMode.dark;
+    //   case 'system':
+    //     return ThemeMode.system;
+    //   default:
+    //     return ThemeMode.system;
+    // }
+
+    return ThemeMode.values[json['theme'] as int];
   }
 
   @override
   Map<String, dynamic>? toJson(ThemeMode state) {
-    // TODO: implement toJson
-    throw UnimplementedError();
+    // final v = switch (state) {
+    //   ThemeMode.light => 'light',
+    //   ThemeMode.dark => 'dark',
+    //   ThemeMode.system => 'system',
+    // };
+    // return {'theme': v};
+
+    return {'theme': state.index};
   }
 }
