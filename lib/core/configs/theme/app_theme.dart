13b5963b1e318b1c7b6bfbfde84f05a8030db58e
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-16 17:03:12 +0200

    Add input decoration theme to app themes

--- lib/core/configs/theme/app_theme.dart
+++ lib/core/configs/theme/app_theme.dart
@@ -7,6 +7,33 @@ class AppTheme {
     primaryColor: AppColors.primary,
     fontFamily: 'Satoshi',
     scaffoldBackgroundColor: AppColors.lightBackground,
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.transparent,
+      contentPadding: EdgeInsets.all(30),
+      hintStyle: TextStyle(
+        color: Color(0xff383838),
+        fontWeight: FontWeight.w400,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: BorderSide(
+          color: Colors.black.withOpacity(0.4),
+          width: 0.6,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(
+          color: AppColors.primary,
+          width: 1.2,
+        ),
+      ),
+      // border: OutlineInputBorder(
+      //   borderRadius: BorderRadius.circular(30),
+      //   borderSide: BorderSide(color: Colors.green, width: 0.4),
+      // ),
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
@@ -27,6 +54,33 @@ class AppTheme {
     primaryColor: AppColors.primary,
     fontFamily: 'Satoshi',
     scaffoldBackgroundColor: AppColors.darkBackground,
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.transparent,
+      contentPadding: EdgeInsets.all(30),
+      hintStyle: TextStyle(
+        color: Color(0xffA7A7A7),
+        fontWeight: FontWeight.w400,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: BorderSide(
+          color: Colors.white.withOpacity(0.4),
+          width: 0.4,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: const BorderSide(
+          color: AppColors.primary,
+          width: 1.2,
+        ),
+      ),
+      // border: OutlineInputBorder(
+      //   borderRadius: BorderRadius.circular(30),
+      //   borderSide: BorderSide(color: Colors.white, width: 0.4),
+      // ),
+    ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
