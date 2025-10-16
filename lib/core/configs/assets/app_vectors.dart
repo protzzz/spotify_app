ab8191e1fd59ae95b66fb5d0dffee1959991ff2d
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-16 17:02:57 +0200

    Add new vector asset paths for icons

--- lib/core/configs/assets/app_vectors.dart
+++ lib/core/configs/assets/app_vectors.dart
@@ -7,4 +7,7 @@ class AppVectors {
   static const String sun = '${basePath}sun$format';
   static const String topLines = '${basePath}top-lines$format';
   static const String bottomLines = '${basePath}bottom-lines$format';
+  static const String appleIcon = '${basePath}apple-icon$format';
+  static const String googleIcon = '${basePath}google-icon$format';
+  static const String closedEye = '${basePath}close-eye$format';
 }
